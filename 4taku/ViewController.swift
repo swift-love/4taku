57d66408034993de87f5fb28038cb23bf42af9ca
Satoru Ohguchi <s.ohguchi@gmail.com>
2018-02-03 18:09:10 +0900

    Initial Commit

---
+++ 4taku/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  4taku
+//
+//  Created by Satoru Ohguchi on 2018/02/03.
+//  Copyright © 2018年 Satoru Ohguchi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
