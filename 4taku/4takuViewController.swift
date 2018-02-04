69ddb878fa2b534a423cfa6f7cf283a1de1cb036
Satoru Ohguchi <s.ohguchi@gmail.com>
2018-02-04 17:33:36 +0900

    first commit

---
+++ 4taku/4takuViewController.swift
@@ -0,0 +1,9 @@
+//
+//  4takuViewController.swift
+//  4taku
+//
+//  Created by Satoru Ohguchi on 2018/02/03.
+//  Copyright © 2018年 Satoru Ohguchi. All rights reserved.
+//
+
+import Foundation
---
+++ 4taku/ResultViewContoller.swift
@@ -0,0 +1,9 @@
+//
+//  ResultViewContoller.swift
+//  4taku
+//
+//  Created by Satoru Ohguchi on 2018/02/03.
+//  Copyright © 2018年 Satoru Ohguchi. All rights reserved.
+//
+
+import Foundation
