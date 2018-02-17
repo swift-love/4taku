b5a34ca4f9e7af12d16207aa0a9e8a4865c050e3
Satoru Ohguchi <s.ohguchi@gmail.com>
2018-02-18 03:05:17 +0900

    uiWindSegueに変更

--- 4taku/ViewController.swift
+++ 4taku/ViewController.swift
@@ -15,6 +15,10 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    @IBAction func unWindSegue(_ sender: UIStoryboardSegue) {
+        print("topwindow")
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
